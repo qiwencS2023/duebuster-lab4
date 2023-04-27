42d1ce6773231745b1dded5cada93747dfceb6ca
Lin Xi <yxiang2@andrew.cmu.edu>
2023-04-26 20:59:12 -0700

    change to proto

--- storage/main.go
+++ storage/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -14,14 +11,4 @@ func main() {
 		return
 	}
 
-	router := mux.NewRouter()
-	router.HandleFunc("/register", register).Methods("POST")
-	router.HandleFunc("/createTable", createTable).Methods("POST")
-	router.HandleFunc("/deleteTable", deleteTable).Methods("DELETE")
-	router.HandleFunc("/deleteLine", deleteLine).Methods("DELETE")
-	router.HandleFunc("/insertLine", insertLine).Methods("POST")
-	router.HandleFunc("/updateLine", updateLine).Methods("PUT")
-
-	port, _ := strconv.Atoi(os.Args[1])
-	http.ListenAndServe(":"+strconv.Itoa(port), router)
 }
